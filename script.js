6c2e0c94ec9d62a84dcfaf2a78519ef5ca276d1b
Wesley Victor <ato.business01@gmail.com>
2025-12-08 17:22:46 +0000

    Commit n. 46: Organização do modo harburguer

--- script.js
+++ script.js
@@ -1,14 +1,48 @@
-const btnToggle = document.getElementById("toggleDepoimentos");
-const depoimentos = document.getElementById("depoimentos");
+const btnTopo = document.getElementById("btn-topo");
 
-btnToggle.addEventListener("click", () => {
-    depoimentos.classList.toggle("expandido");
-
-    if (depoimentos.classList.contains("expandido")) {
-      btnToggle.textContent = "Ver Menos";
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 250) {
+        btnTopo.style.opacity = "1";
+        btnTopo.style.pointerEvents = "auto";
     } else {
-      btnToggle.textContent = "Ver Mais";
-      // scroll suave pra voltar a seção ao topo
-      depoimentos.scrollIntoView({ behavior: "smooth", block: "start" });
+        btnTopo.style.opacity = "0";
+        btnTopo.style.pointerEvents = "none";
     }
 });
+
+btnTopo.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+
+const hamburger = document.getElementById("hamburger");
+const menu = document.getElementById("menu");
+const nav = document.querySelector("nav");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("ativo");
+    menu.classList.toggle("aberto");
+});
+
+// Fecha ao clicar em qualquer link
+document.querySelectorAll(".menu a").forEach(link => {
+    link.addEventListener("click", () => {
+        fecharMenu();
+    });
+});
+
+// Fecha ao clicar fora do nav
+document.addEventListener("click", (e) => {
+    const clicouFora = !nav.contains(e.target);
+
+    if (clicouFora && menu.classList.contains("aberto")) {
+        fecharMenu();
+    }
+});
+
+function fecharMenu() {
+    menu.classList.remove("aberto");
+    hamburger.classList.remove("ativo");
+}
